20fd8387d298b3eb47ce1f82390274d803b5d4a1
王梓铭 <wangziming12598@apexsoft.com.cn>
2024-09-03 16:13:46 +0800

    first commit

---
+++ src/test/java/com/wzm/aio/AioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzm.aio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
