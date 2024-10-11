95ead5032fb8dd6e87d24f38fbdf909d8776b0ce
王梓铭 <wangziming707@gmial.com>
2024-10-11 16:44:30 +0800

    小修改

--- src/main/java/com/wzm/aio/AioApplication.java
+++ src/main/java/com/wzm/aio/AioApplication.java
@@ -5,97 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Flow;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.Future;
-import java.util.function.Consumer;
-
 //新增系统配置管理模块
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @ConfigurationPropertiesScan({"com.wzm.aio.properties"})
 public class AioApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(AioApplication.class, args);
     }
 
-}
-
-class OneShotPublisher implements Flow.Publisher<Boolean> {
-    private final ExecutorService executor = ForkJoinPool.commonPool(); // daemon-based
-    private boolean subscribed; // true after first subscribe
-
-    public synchronized void subscribe(Flow.Subscriber<? super Boolean> subscriber) {
-        if (subscribed)
-            subscriber.onError(new IllegalStateException()); // only one allowed
-        else {
-            subscribed = true;
-            subscriber.onSubscribe(new OneShotSubscription(subscriber, executor));
-        }
-    }
-
-    static class OneShotSubscription implements Flow.Subscription {
-        private final Flow.Subscriber<? super Boolean> subscriber;
-        private final ExecutorService executor;
-        private Future<?> future; // to allow cancellation
-        private boolean completed;
-
-        OneShotSubscription(Flow.Subscriber<? super Boolean> subscriber, ExecutorService executor) {
-            this.subscriber = subscriber;
-            this.executor = executor;
-        }
-
-        public synchronized void request(long n) {
-            if (!completed) {
-                completed = true;
-                if (n <= 0) {
-                    IllegalArgumentException ex = new IllegalArgumentException();
-                    executor.execute(() -> subscriber.onError(ex));
-                } else {
-                    future = executor.submit(() -> {
-                        subscriber.onNext(Boolean.TRUE);
-                        subscriber.onComplete();
-                    });
-                }
-            }
-        }
-
-        public synchronized void cancel() {
-            completed = true;
-            if (future != null) future.cancel(false);
-        }
-    }
-}
-
-class SampleSubscriber<T> implements Flow.Subscriber<T> {
-    final Consumer<? super T> consumer;
-    Flow.Subscription subscription;
-    final long bufferSize;
-    long count;
-
-    SampleSubscriber(long bufferSize, Consumer<? super T> consumer) {
-        this.bufferSize = bufferSize;
-        this.consumer = consumer;
-    }
-
-    public void onSubscribe(Flow.Subscription subscription) {
-        long initialRequestSize = bufferSize;
-        count = bufferSize - bufferSize / 2; // re-request when half consumed
-        (this.subscription = subscription).request(initialRequestSize);
-    }
-
-    public void onNext(T item) {
-        if (--count <= 0) subscription.request(count = bufferSize - bufferSize / 2);
-        consumer.accept(item);
-    }
-
-    public void onError(Throwable ex) {
-        ex.printStackTrace();
-    }
-
-    public void onComplete() {
-    }
 }
