0ce918376f150c026560e7fd095eab18164fabbc
王梓铭 <wangziming707@gmial.com>
2024-12-05 17:01:18 +0800

    继续

--- src/main/resources/server/main-server/main.sh
+++ src/main/resources/server/main-server/main.sh
@@ -1,4 +1,4 @@
-# ubuntu镜像 https://mirrors.aliyun.com/ubuntu/
+
 # 设置root密码并允许root ssh远程登录
 
 sudo passwd root
@@ -27,4 +27,7 @@ df -h # 检查挂载是否成功
 nano /lib/systemd/system/docker.service
 # 在原来的位置改为：
 # ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock -H tcp://0.0.0.0:2375 -H unix://var/run/docker.sock
+systemctl daemon-reload
+systemctl restart docker
 
+# gitlab docker禁用 ssh密钥登录
---
+++ src/main/resources/server/public/main.sh
@@ -0,0 +1,20 @@
+# ubuntu镜像 https://mirrors.aliyun.com/ubuntu/
+# 设置root密码，和启动root ssh
+sudo passwd root
+su root
+nano /etc/ssh/sshd_config
+# 解决内存buffer/cache占用过高问题
+nano /etc/sysctl.conf
+#将 PermitRootLogin 设置为 yes
+systemctl restart sshd
+# 使用下面配置
+vm.dirty_ratio = 5
+vm.dirty_background_ratio =5
+vm.dirty_writeback_centisecs=100
+vm.dirty_expire_centisecs=30000
+vm.drop_caches=3
+vm.swappiness=100
+vm.vfs_cache_pressure=133
+
+sysctl -p # 使配置生效
+
